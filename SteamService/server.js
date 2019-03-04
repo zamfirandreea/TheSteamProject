7414283471132b63651a0487db7bbd5587ecdc35
agherasim <agherasim@pentalog.com>
2019-03-04 11:27:15 +0200

    treated error code responses

--- SteamService/server.js
+++ SteamService/server.js
@@ -2,37 +2,46 @@ const http = require('http');
 const axios = require('axios');
 
 const STEAM_API_BASE_URL = 'http://api.steampowered.com';
+const PORT= 8081;
 
-function responseForTestEndpoint(res) {
-    console.log("Test endpoint called!");
+const respondForTestEndpoint = res => {
+  console.log("Test endpoint called!");
   res.write(JSON.stringify({
     status: "SERVER UP!"
   }));
   res.end();
-}
+};
 
-function responseForOptions(res) {
-    console.log("OPTIONS endpoint called!");
+const respondForOptions= res => {
+  console.log("OPTIONS endpoint called!");
   res.write(JSON.stringify({
     status: "Options Call detected!"
   }));
   res.end();
-}
+};
 
-function responseForGet(path, res) {
+const respondWithError = (error, res) => {
+  console.log("error =>", JSON.stringify(error.message));
+  res.statusCode = error.response.status;
+  res.statusMessage = error.response.statusText;
+  res.end();
+};
+
+const respondForGet = (path, res) => {
   console.log("GET endpoint called!");
 
   axios({
-    method:"GET",
+    method: "GET",
     url: STEAM_API_BASE_URL + path,
     headers: {'Content-Type': 'application/json; charset=UTF-8'}
   }).then((response) => {
     console.log("STEAM RESPONSE => ", JSON.stringify(response.data));
     res.write(JSON.stringify(response.data));
     res.end();
-  });
-}
-function responseWithPayload(method, path, payload, res) {
+  }).catch(error => respondWithError(error, res));
+};
+
+const respondWithPayload = (method, path, payload, res) => {
 
   console.log(`${method} endpoint called!`);
   console.log(JSON.stringify(payload));
@@ -46,40 +55,42 @@ function responseWithPayload(method, path, payload, res) {
     console.log("STEAM RESPONSE => ", JSON.stringify(response.data));
     res.write(JSON.stringify(response.data));
     res.end();
-  });
-}
+  }).catch(error => respondWithError(error, res));
+
+};
 
 //create a server object:
-let server = http.createServer(function (req, res) {
+const server = http.createServer(function (req, res) {
 
-    let {method, url: path} = req;
-    let body = [];
-    let payload = {};
+  console.log("Service called!");
+  let {method, url: path} = req;
+  let body = [];
+  let payload = {};
 
-    console.log({method, path});
+  console.log({method, path});
 
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Headers','*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
 
-  if(method === "GET" && path==="/test") {
-      responseForTestEndpoint(res);
-  }else if(method === "OPTIONS"){
-    responseForOptions(res);
-  }else if(method === "GET"){
-      responseForGet(path, res);
-    }else{
-      req.on('error', (err) => {
-        console.error(err);
-      }).on('data', (chunk) => {
-        body.push(chunk);
-      }).on('end', () => {
-        if (body.length) {
-          payload = JSON.parse(Buffer.concat(body).toString());
-        }
-        responseWithPayload(method, path, payload, res);
-      });
+  if (method === "GET" && path === "/test") {
+    respondForTestEndpoint(res);
+  } else if (method === "OPTIONS") {
+    respondForOptions(res);
+  } else if (method === "GET") {
+    respondForGet(path, res);
+  } else {
+    req.on('error', (err) => {
+      console.error(err);
+    }).on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      if (body.length) {
+        payload = JSON.parse(Buffer.concat(body).toString());
+      }
+      respondWithPayload(method, path, payload, res);
+    });
 
-    }
+  }
 
 });
-server.listen(8081); //the server object listens on port 8081
+server.listen(PORT);
