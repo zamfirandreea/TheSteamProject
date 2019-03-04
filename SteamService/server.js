76b43682e9de0e0f1040f2bebc21228be79ba84b
agherasim <agherasim@pentalog.com>
2019-03-04 10:14:50 +0200

    removed npm-debul.log file (Should not be committed) updated the SteamService to work with payload and to allow CORS

--- SteamService/server.js
+++ SteamService/server.js
@@ -1,34 +1,85 @@
 const http = require('http');
 const axios = require('axios');
 
+const STEAM_API_BASE_URL = 'http://api.steampowered.com';
+
+function responseForTestEndpoint(res) {
+    console.log("Test endpoint called!");
+  res.write(JSON.stringify({
+    status: "SERVER UP!"
+  }));
+  res.end();
+}
+
+function responseForOptions(res) {
+    console.log("OPTIONS endpoint called!");
+  res.write(JSON.stringify({
+    status: "Options Call detected!"
+  }));
+  res.end();
+}
+
+function responseForGet(path, res) {
+  console.log("GET endpoint called!");
+
+  axios({
+    method:"GET",
+    url: STEAM_API_BASE_URL + path,
+    headers: {'Content-Type': 'application/json; charset=UTF-8'}
+  }).then((response) => {
+    console.log("STEAM RESPONSE => ", JSON.stringify(response.data));
+    res.write(JSON.stringify(response.data));
+    res.end();
+  });
+}
+function responseWithPayload(method, path, payload, res) {
+
+  console.log(`${method} endpoint called!`);
+  console.log(JSON.stringify(payload));
+
+  axios({
+    method,
+    url: STEAM_API_BASE_URL + path,
+    headers: {'Content-Type': 'application/json; charset=UTF-8'},
+    data: payload
+  }).then((response) => {
+    console.log("STEAM RESPONSE => ", JSON.stringify(response.data));
+    res.write(JSON.stringify(response.data));
+    res.end();
+  });
+}
+
 //create a server object:
 let server = http.createServer(function (req, res) {
 
     let {method, url: path} = req;
     let body = [];
     let payload = {};
-    req.on('error', (err) => {
+
+    console.log({method, path});
+
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers','*');
+
+  if(method === "GET" && path==="/test") {
+      responseForTestEndpoint(res);
+  }else if(method === "OPTIONS"){
+    responseForOptions(res);
+  }else if(method === "GET"){
+      responseForGet(path, res);
+    }else{
+      req.on('error', (err) => {
         console.error(err);
-    }).on('data', (chunk) => {
+      }).on('data', (chunk) => {
         body.push(chunk);
-    }).on('end', () => {
-        if(body.length) {
-            payload = JSON.parse(Buffer.concat(body).toString());
+      }).on('end', () => {
+        if (body.length) {
+          payload = JSON.parse(Buffer.concat(body).toString());
         }
-        console.log(method, path);
-        console.log(JSON.stringify(payload));
+        responseWithPayload(method, path, payload, res);
+      });
 
-        axios({
-            method,
-            url: 'http://api.steampowered.com' + path,
-            headers: {'Content-Type': 'application/json; charset=UTF-8'}
-        }).then((response) => {
-            console.log("STEAM RESPONSE => ", JSON.stringify(response.data));
-            res.write(JSON.stringify(response.data)); //write a response to the client
-            res.end(); //end the response
-        });
-    });
-
+    }
 
 });
 server.listen(8081); //the server object listens on port 8081
