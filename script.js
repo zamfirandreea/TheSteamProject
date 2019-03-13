1be77b73b3a0bee213da5e3dd56322ab2880961b
Andreea Zamfir <zamfir.andreea.maria@gmail.com>
2019-03-13 21:18:56 +0200

    Commit new branch for testing a new approach

--- script.js
+++ script.js
@@ -1,168 +1,251 @@
-//
-// const steamKey = '0DB8EC376B48E0736AB221887E5C7B6D';
-// const steamProfileId = '76561198118730252';
-// const header = document.getElementsByClassName('header')[0];
-// const content = document.getElementsByClassName('left_side')[0];
-//
-//
-// async function getResponseProfileDetails() {
-//     try {
-//         const profileDetailsRes = await axios.get('http://localhost:8081/ISteamUser/GetPlayerSummaries/v0002/?key=' + steamKey + '&steamids=' + steamProfileId);
-//         const levelRes = await axios.get('http://localhost:8081/IPlayerService/GetSteamLevel/v1/?key=' + steamKey + '&steamid=' + steamProfileId);
-//         const recentlyPlayedRes = await axios.get('http://localhost:8081/IPlayerService/GetRecentlyPlayedGames/v0001/?key=' + steamKey + '&steamid=' + steamProfileId + '&format=json');
-//         const ownedGamesRes = await axios.get('http://localhost:8081/IPlayerService/GetOwnedGames/v0001/?key=' + steamKey + '&steamid=' + steamProfileId + '&format=json&include_appinfo=1&include_&include_played_free_games=1');
-//
-//         getProfilePicture(profileDetailsRes.data);
-//         getProfileName(profileDetailsRes.data);
-//         getProfileLevel(levelRes.data);
-//         getRecentlyPlayedGame(recentlyPlayedRes.data);
-//         getOwnedGames(ownedGamesRes.data);
-//         // getFriendsList(friendsRes.data);
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
-//
-// getResponseProfileDetails();
-//
-// const getProfilePicture = (res) => {
-//     const imgSection = document.createElement('section');
-//     imgSection.className = 'image_section';
-//     const avatar = document.createElement('img');
-//     avatar.className = 'profile_picture';
-//     avatar.src = res.response.players[0].avatarfull;
-//     imgSection.appendChild(avatar);
-//     header.appendChild(imgSection);
-// };
-//
-// const getProfileName = (res) => {
-//     const detailsSection = document.createElement('section');
-//
-//     const name = document.createElement('p');
-//
-//     detailsSection.className = 'details_section';
-//     name.className = 'profile_name';
-//
-//     name.innerHTML = 'Player Username: ' + res.response.players[0].personaname;
-//
-//     detailsSection.appendChild(name);
-//
-//     if(!!res.response.players[0].realname) {
-//         const realName = document.createElement('p');
-//         realName.className = 'profile_name';
-//         realName.innerHTML = 'Player Name: ' + res.response.players[0].realname;
-//         detailsSection.appendChild(realName);
-//     }
-//
-//     header.appendChild(detailsSection);
-// };
-//
-// const getProfileLevel = (res) => {
-//     const detailsSection = document.getElementsByClassName('details_section')[0];
-//     const level = document.createElement('p');
-//     const levelValue = document.createElement('span');
-//     level.className = 'profile_level';
-//     levelValue.innerHTML = res.response.player_level;
-//     levelValue.className = 'level_value';
-//     level.innerHTML = 'Level ';
-//     level.appendChild(levelValue);
-//     detailsSection.appendChild(level);
-//     header.appendChild(detailsSection);
-// };
-//
-// const gameDisplay = (game, section) => {
-//     const gameImage = document.createElement('img');
-//     const gameName = document.createElement('p');
-//     const gamePlaytime = document.createElement('p');
-//     gameImage.className = 'game_image';
-//     gameName.className = 'game_name';
-//     gamePlaytime.className = 'game_playtime';
-//
-//     gameImage.src = 'http://media.steampowered.com/steamcommunity/public/images/apps/' + game.appid + '/' + game.img_logo_url + '.jpg';
-//     gameName.innerHTML = game.name;
-//     gamePlaytime.innerHTML = 'Total playtime: ' + sec2time(game.playtime_forever);
-//
-//     section.appendChild(gameImage);
-//     section.appendChild(gameName);
-//     section.appendChild(gamePlaytime);
-// };
-//
-// const getRecentlyPlayedGame = (res) => {
-//     const recentGameSection = document.createElement('section');
-//     const header = document.createElement('h2');
-//     header.className = 'recent_game_header';
-//     header.innerHTML = 'Recent Activity';
-//     recentGameSection.appendChild(header);
-//     gameDisplay(res.response.games[0], recentGameSection);
-//     content.appendChild(recentGameSection);
-// };
-//
-// const getOwnedGames = (res) => {
-//     const ownedGameSection = document.createElement('section');
-//     const header = document.createElement('h2');
-//     ownedGameSection.className = 'owned_games_section';
-//     header.className = 'owned_games_header';
-//     header.innerHTML = 'Owned Games (Top 10 most played)';
-//     ownedGameSection.appendChild(header);
-//     const games = res.response.games;
-//     games.sort((game01, game02) => game02.playtime_forever - game01.playtime_forever);
-//     games.slice(0,10).forEach(game => {
-//         const gameSection = document.createElement('section');
-//         gameSection.className = 'game_section';
-//         gameDisplay(game, gameSection);
-//         ownedGameSection.appendChild(gameSection);
-//     });
-//
-//     content.appendChild(ownedGameSection);
-// };
-// const sec2time = (timeInSeconds) => {
-//     const pad = (num, size) => ('000' + num).slice(size * -1),
-//         time = parseFloat(timeInSeconds).toFixed(3),
-//         hours = Math.floor(time / 60 / 60),
-//         minutes = Math.floor(time / 60) % 60,
-//         seconds = Math.floor(time - minutes * 60);
-//
-//     return pad(hours, 2) + ':' + pad(minutes, 2) + ':' + pad(seconds, 2);
-// };
+const steamKey = '0DB8EC376B48E0736AB221887E5C7B6D';
+let steamProfileId = '76561198118730252';
+const header = document.getElementsByClassName('header')[0];
+const leftSection = document.getElementsByClassName('left_side')[0];
 
-// const getFriendsList = (res, profileDetailsRes) => {
-//     const friendSection = document.createElement('section');
-//     const header = document.createElement('h2');
-//     friendSection.className = 'friend_section';
-//     header.className = 'friends_header';
-//
-//     header.innerHTML = 'Friends';
-//
-//     friendSection.appendChild(header);
-//     aside.appendChild(friendSection);
-// };
+const getProfileAvatar = async (steamId) => {
+    initProfileAvatar();
+    const profileAvatar = await getProfileAvatarData(steamId);
+    updateProfileAvatar(profileAvatar);
+    // console.log(profileAvatar);
+};
 
-// const createSteamIdInput = () => {
-//   const idInput = document.createElement('input');
-//   idInput.type = 'text';
-//   idInput.id = 'id_input';
-//   idInput.defaultValue = '76561198118730252';
-//   document.getElementsByClassName('steam_id_field')[0].appendChild(idInput);
-//   console.log(idInput.value);
-// };
-//
-// const submitButton = () => {
-//     const button = document.createElement('button');
-//     button.className = 'submit_btn';
-//     button.innerHTML = 'Submit';
-//     button.onclick = onClick;
-//     document.getElementsByClassName('steam_id_field')[0].appendChild(button);
-// };
-//
-// const onClick = () => {
-//     console.log('button Clicked!');
-//     const urlInput = document.getElementById('id_input');
-//     const container = document.getElementsByClassName('container')[0];
-//     container.innerHTML = '';
-//     console.log(urlInput.value);
-//
-// };
-//
-// createSteamIdInput();
-// submitButton();
+const createProfileAvatar = () => {
+    const imgSection = document.createElement('section');
+    imgSection.className = 'image_section';
+    const avatar = document.createElement('img');
+    avatar.className = 'profile_picture';
+    imgSection.appendChild(avatar);
+    header.appendChild(imgSection);
+    return imgSection;
+};
 
+const findProfileAvatar = () => {
+    return document.getElementsByClassName('image_section')[0];
+};
+
+const initProfileAvatar = () => {
+    // console.log(createProfileAvatar());
+    return findProfileAvatar() || createProfileAvatar();
+};
+
+const getProfileAvatarData = async (steamProfileId) => {
+    const response = await axios.get('http://localhost:8081/ISteamUser/GetPlayerSummaries/v0002/?key=' + steamKey + '&steamids=' + steamProfileId);
+    // console.log(response.data.response.players[0].avatarfull);
+    return response.data.response.players[0].avatarfull;
+};
+
+const updateProfileAvatar = (avatarData) => {
+    const avatar = document.getElementsByClassName('profile_picture')[0];
+    avatar.src = avatarData;
+    // console.log(avatar.src);
+};
+
+const getProfileName = async (steamId) => {
+    initProfileName();
+    const profileName = await getProfileNameData(steamId);
+    updateProfileName(profileName);
+};
+
+const createProfileName = () => {
+    const detailsSection = document.createElement('section');
+    detailsSection.className = 'details_section';
+    const name = document.createElement('p');
+    name.className = 'profile_name';
+    detailsSection.appendChild(name);
+    header.appendChild(detailsSection);
+    return detailsSection;
+};
+
+const findProfileName = () => {
+    return document.getElementsByClassName('profile_name')[0];
+};
+
+const initProfileName = () => {
+    // console.log(createProfileName());
+    return findProfileName() || createProfileName();
+};
+
+const getProfileNameData = async (steamProfileId) => {
+    const response = await axios.get('http://localhost:8081/ISteamUser/GetPlayerSummaries/v0002/?key=' + steamKey + '&steamids=' + steamProfileId);
+    // console.log(response.data.response.players[0].personaname);
+    return response.data.response.players[0].personaname;
+};
+
+const updateProfileName = (nameData) => {
+    const name = document.getElementsByClassName('profile_name')[0];
+    name.innerHTML = nameData;
+    // console.log(name.innerHTML);
+};
+
+const getProfileLevel = async (steamId) => {
+    const profileLevel = initProfileLevel();
+    const data = await getProfileLevelData(steamId);
+    updateProfileLevel(profileLevel, data);
+};
+
+const createProfileLevel = () => {
+    const detailsSection = document.getElementsByClassName('details_section')[0];
+    const level = document.createElement('p');
+    level.className = 'profile_level';
+    detailsSection.appendChild(level);
+    return level;
+};
+
+const findProfileLevel = () => {
+    const foundElements = document.getElementsByClassName('profile_level');
+    if(foundElements && foundElements.length>0){
+        return foundElements[0];
+    }
+    return null;
+};
+
+const initProfileLevel = () => {
+    // console.log(createProfileLevel());
+    return findProfileLevel() || createProfileLevel();
+};
+
+const getProfileLevelData = async (steamProfileId) => {
+    const response = await axios.get('http://localhost:8081/IPlayerService/GetSteamLevel/v1/?key=' + steamKey + '&steamid=' + steamProfileId);
+    // console.log(response.data.response.player_level);
+    return 'Level ' + response.data.response.player_level;
+};
+
+const updateProfileLevel = (level, levelData) => {
+    level.innerHTML = levelData;
+};
+
+const getRecentPlayedGame = async (steamId) => {
+    const game =  initRecentPlayedGame();
+    const data = await getRecentPlayedGameData(steamId);
+    updateRecentPlayedGame(game, data);
+};
+
+const createRecentPlayedGame = () => {
+    const section = document.createElement('section');
+    section.className = 'recent_game';
+    const header = document.createElement('h2');
+    header.className = 'section_header';
+    header.innerHTML = 'Recent Game Played';
+    section.appendChild(header);
+    leftSection.appendChild(section);
+
+    const gameImage = document.createElement('img');
+    gameImage.className = 'game_image';
+    const gameName = document.createElement('p');
+    gameName.className = 'game_name';
+    const gamePlaytime = document.createElement('p');
+    gamePlaytime.className = 'game_playtime';
+    section.appendChild(gameImage);
+    section.appendChild(gameName);
+    section.appendChild(gamePlaytime);
+    return section;
+};
+
+const findRecentPlayedGame = () => {
+    const foundElements = document.getElementsByClassName('recent_game');
+    if(foundElements && foundElements.length>0){
+        return foundElements[0];
+    }
+    return null;
+};
+
+const initRecentPlayedGame = () => {
+    return findRecentPlayedGame() || createRecentPlayedGame();
+};
+
+const getRecentPlayedGameData = async (steamProfileId) => {
+    const response = await axios.get('http://localhost:8081/IPlayerService/GetRecentlyPlayedGames/v0001/?key=' + steamKey + '&steamid=' + steamProfileId + '&format=json');
+    // console.log(response.data.response.games[0]);
+    return response.data.response.games[0];
+};
+
+const updateRecentPlayedGame = (game, gameData) => {
+    const gameImage = document.getElementsByClassName('game_image')[0];
+    const gameName = document.getElementsByClassName('game_name')[0];
+    const gamePlaytime = document.getElementsByClassName('game_playtime')[0];
+
+    gameImage.src = 'http://media.steampowered.com/steamcommunity/public/images/apps/' + gameData.appid + '/' + gameData.img_logo_url + '.jpg';
+    gameName.innerHTML = gameData.name;
+    gamePlaytime.innerHTML = 'Total playtime: ' + sec2time(gameData.playtime_forever);
+};
+
+const sec2time = (timeInSeconds) => {
+    const pad = (num, size) => ('000' + num).slice(size * -1),
+        time = parseFloat(timeInSeconds).toFixed(3),
+        hours = Math.floor(time / 60 / 60),
+        minutes = Math.floor(time / 60) % 60,
+        seconds = Math.floor(time - minutes * 60);
+
+    return pad(hours, 2) + ':' + pad(minutes, 2) + ':' + pad(seconds, 2);
+};
+
+const getOwnedGames = async (steamId) => {
+    const game = initOwnedGames();
+    const gameData = await getOwnedGamesData(steamId);
+    updateOwnedGames(game, gameData);
+};
+
+const createOwnedGames = () => {
+    const section = document.createElement('section');
+    section.className = 'owned_games';
+    const header = document.createElement('h2');
+    header.className = 'section_header';
+    header.innerHTML = 'Owned Games (Top 10 Played)';
+    section.appendChild(header);
+
+    const gameImage = document.createElement('img');
+    gameImage.className = 'owned_game_image';
+    const gameName = document.createElement('p');
+    gameName.className = 'owned_game_name';
+    const gamePlaytime = document.createElement('p');
+    gamePlaytime.className = 'owned_game_playtime';
+    section.appendChild(gameImage);
+    section.appendChild(gameName);
+    section.appendChild(gamePlaytime);
+    return section;
+};
+
+const findOwnedGames = () => {
+    const foundElements =  document.getElementsByClassName('owned_games')[0];
+    if(foundElements && foundElements.length>0){
+        return foundElements[0];
+    }
+    return null;
+};
+
+const initOwnedGames =() => {
+    return findOwnedGames() || createOwnedGames();
+};
+
+const getOwnedGamesData = async (steamProfileId) => {
+    const response = await axios.get('http://localhost:8081/IPlayerService/GetOwnedGames/v0001/?key=' + steamKey + '&steamid=' + steamProfileId + '&format=json&include_appinfo=1&include_&include_played_free_games=1');
+    const ownedGames = response.data.response.games;
+    ownedGames.sort((game01, game02) => game02.playtime_forever - game01.playtime_forever);
+    return ownedGames.slice(0,10);
+};
+
+const updateOwnedGames = (game, gameData) => {
+    const gameImage = document.getElementsByClassName('owned_game_image');
+    const gameName = document.getElementsByClassName('owned_game_name');
+    const gamePlaytime = document.getElementsByClassName('owned_game_playtime');
+
+    gameImage.src = 'http://media.steampowered.com/steamcommunity/public/images/apps/' + gameData.appid + '/' + gameData.img_logo_url + '.jpg';
+    gameName.innerHTML = gameData.name;
+    console.log(gameName.innerHTML);
+    gamePlaytime.innerHTML = 'Total Playtime: ' + sec2time(gameData.playtime_forever);
+};
+
+async function getResponseProfileDetails() {
+    try {
+        getProfileAvatar(steamProfileId);
+        getProfileName(steamProfileId);
+        getProfileLevel(steamProfileId);
+        getRecentPlayedGame(steamProfileId);
+        getOwnedGames(steamProfileId);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getResponseProfileDetails();
